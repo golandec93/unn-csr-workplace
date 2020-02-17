d6d4be0cfd52f2de2627e93bf3fc4a667d662ecd
golandec93 <kasparf93@gmail.com>
2020-02-17 14:31:03 +0300

    fix problem with page data update on broken service call

--- src/app/service-details/service-details.component.ts
+++ src/app/service-details/service-details.component.ts
@@ -27,17 +27,12 @@ export class ServiceDetailsComponent implements OnInit {
   private equipment$: Observable<Equipment[]>;
 
   ngOnInit() {
-    const service = this.activatedRoute.paramMap.pipe(
+    this.service$ = this.activatedRoute.paramMap.pipe(
       switchMap((params: ParamMap): Observable<Service> => {
         const serviceId = params.get('serviceId');
         return this.servicesService.getServiceById(parseInt(serviceId, 10));
       }),
       share());
-    this.updateDataForService(service);
-  }
-
-  private updateDataForService(initialService: Observable<Service>) {
-    this.service$ = initialService;
     this.service$.subscribe(service => this.service = service);
     this.equipment$ = this.service$.pipe(
       switchMap(service => this.equipmentService.getEquipmentByServiceId(service.id)),
@@ -48,7 +43,16 @@ export class ServiceDetailsComponent implements OnInit {
 
   public markServiceAsBroken() {
     const hardwareId = this.equipment[Math.floor(Math.random() * this.equipment.length)].id;
-    this.equipmentService.markAsBroken(hardwareId)
-      .subscribe(_ => this.updateDataForService(this.service$));
+    this.equipmentService.markAsBroken(hardwareId).subscribe(
+      _ => {
+        this.service$ = this.servicesService.getServiceById(this.service.id).pipe(share());
+        this.service$.subscribe(service => this.service);
+        this.equipment$ = this.service$.pipe(
+          switchMap(service => this.equipmentService.getEquipmentByServiceId(service.id)),
+          tap(console.log),
+          share());
+        this.equipment$.subscribe(equipment => this.equipment = equipment);
+      }
+    );
   }
 }
